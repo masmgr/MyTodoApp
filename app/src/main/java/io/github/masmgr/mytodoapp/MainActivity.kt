9f83ccadad46239291374a346d3508de9a3a0b92
imasaki20 <imasaki20@gmail.com>
2020-07-18 14:27:24 +0900

    Todoリストの実装。

--- app/src/main/java/io/github/masmgr/mytodoapp/MainActivity.kt
+++ app/src/main/java/io/github/masmgr/mytodoapp/MainActivity.kt
@@ -1,11 +1,32 @@
 package io.github.masmgr.mytodoapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rv =
+            findViewById<View>(R.id.recyclerView) as RecyclerView
+        val adapter = RecyclerViewAdapter(createDataset())
+        val llm = LinearLayoutManager(this)
+        rv.setHasFixedSize(true)
+        rv.layoutManager = llm
+        rv.adapter = adapter
+    }
+
+    private fun createDataset(): List<RowData> {
+        val dataset = mutableListOf<RowData>()
+        (0..49).forEach { i ->
+            dataset += RowData(
+                "タイトル${i}",
+                "詳細${i}"
+            )
+        }
+        return dataset
     }
 }
--- app/src/test/java/io/github/masmgr/mytodoapp/ExampleUnitTest.kt
+++ app/src/test/java/io/github/masmgr/mytodoapp/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package io.github.masmgr.mytodoapp
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
