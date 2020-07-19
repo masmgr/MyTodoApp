208ad3681c04a604a3a81ae93c06e98601c0b209
imasaki20 <imasaki20@gmail.com>
2020-07-19 13:33:10 +0900

    ソースコードのリファクタリング

--- app/src/main/java/io/github/masmgr/mytodoapp/MainActivity.kt
+++ app/src/main/java/io/github/masmgr/mytodoapp/MainActivity.kt
@@ -1,18 +1,13 @@
 package io.github.masmgr.mytodoapp
 
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     private val mainViewModel = MainViewModel()
@@ -24,16 +19,9 @@ class MainActivity : AppCompatActivity() {
 
         adapter = TodoAdapter(
             MutableList(50) { i ->
-                 TodoItem("List ${i+1}", "This is ${i+1} item.", Date())
+                TodoItem("List ${i + 1}", "This is ${i + 1} item.", Date())
             },
-            object : TodoAdapter.ListListener {
-                override fun onClickRow(view: View, rowModel: TodoItem) {
-                    val toast =
-                        Toast.makeText(applicationContext, rowModel.Title, Toast.LENGTH_LONG)
-                    // toast.setGravity(Gravity.TOP, 0, 0)
-                    toast.show()
-                }
-            }
+            TodoListListener(applicationContext)
         )
 
         main_recycler_view.layoutManager = LinearLayoutManager(this)
@@ -47,63 +35,12 @@ class MainActivity : AppCompatActivity() {
             )
         }
 
-        getSwipeToDismissTouchHelper(adapter).attachToRecyclerView(main_recycler_view)
+        ItemTouchHelper(TodoItemTouchHelperCallback(adapter)).attachToRecyclerView(
+            main_recycler_view
+        )
 
         mainViewModel.todo.observe(this, Observer {
             adapter.setItem(it)
         })
     }
-
-    private fun getSwipeToDismissTouchHelper(adapter: TodoAdapter) =
-        ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(
-            ItemTouchHelper.UP or ItemTouchHelper.DOWN or ItemTouchHelper.RIGHT,
-            ItemTouchHelper.RIGHT
-        ) {
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                val fromPosition = viewHolder.adapterPosition
-                val toPosition = target.adapterPosition
-                adapter.move(fromPosition, toPosition)
-                return true
-            }
-
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                adapter.remove(viewHolder.adapterPosition)
-            }
-
-            override fun onChildDraw(
-                c: Canvas,
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                dX: Float,
-                dY: Float,
-                actionState: Int,
-                isCurrentlyActive: Boolean
-            ) {
-                super.onChildDraw(
-                    c,
-                    recyclerView,
-                    viewHolder,
-                    dX,
-                    dY,
-                    actionState,
-                    isCurrentlyActive
-                )
-                val itemView = viewHolder.itemView
-                val background = ColorDrawable()
-                background.color = Color.YELLOW
-                if (dX > 0) {
-                    background.setBounds(
-                        itemView.left,
-                        itemView.top,
-                        itemView.left + dX.toInt(),
-                        itemView.bottom
-                    )
-                }
-                background.draw(c)
-            }
-        })
 }
